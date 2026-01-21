460837a553371b04d73c9c124f88b38d6a9c265d
MichiXVCI <93620387+MichiXVCI@users.noreply.github.com>
2026-01-21 08:28:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("darkToggle").addEventListener("change", e => {
   document.body.classList.toggle("dark-mode", e.target.checked);
 });
+function checkPassword() {
+  const correctPassword = "Sola123"; // CAMBIALA QUI
+  const input = document.getElementById("sitePassword").value;
+
+  if (input === correctPassword) {
+    document.getElementById("login-screen").style.display = "none";
+  } else {
+    document.getElementById("errorMsg").textContent = "Password errata";
+  }
+}
